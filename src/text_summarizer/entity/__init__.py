2aadd2565109bdf9e6219807a1ed8138d4a1cb8d
Nischay Raj <nischayrajmuz17@gmail.com>
2024-03-17 17:59:51 +0530

    data transformation added

--- src/text_summarizer/entity/__init__.py
+++ src/text_summarizer/entity/__init__.py
@@ -14,3 +14,14 @@ class DataValidationConfig:
     root_dir: Path
     STATUS_FILE: str
     ALL_REQUIRED_FILES: list
+
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
+    tokenizer_name: Path
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    data_path: Path
+    model_ckpt: Path
